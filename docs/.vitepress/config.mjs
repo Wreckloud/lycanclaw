eca7ba410a3553d5eadc5a2566d7fa1d48c64ca0
雲之残骸 <1677820334@qq.com>
2026-07-01 11:36:57 +0800

    允许本地在线对战 WebSocket

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
     // 添加Content-Security-Policy
     ['meta', { 
       'http-equiv': 'Content-Security-Policy', 
-      content: "default-src 'self' https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com; style-src 'self' 'unsafe-inline' https://unpkg.com; img-src 'self' data: https: https://secure.gravatar.com; connect-src 'self' https: wss: http: localhost:*; font-src 'self' data:; frame-src 'self' https: http://music.163.com https://music.163.com; media-src 'self' https: http: blob: data:;" 
+      content: "default-src 'self' https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com; style-src 'self' 'unsafe-inline' https://unpkg.com; img-src 'self' data: https: https://secure.gravatar.com; connect-src 'self' https: wss: http: localhost:* ws://127.0.0.1:* ws://localhost:*; font-src 'self' data:; frame-src 'self' https: http://music.163.com https://music.163.com; media-src 'self' https: http: blob: data:;" 
     }],
   ],
 
